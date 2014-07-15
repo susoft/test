1269d278dd3b0fab93bdb3e63b928f633c141d56
susoft <ouyoungdm@gmail.com>
2014-07-15 10:30:14 +0900

    commit

---
+++ SpgrTestSts/src/main/java/com/spgr/dto/TestDto.java
@@ -0,0 +1,27 @@
+package com.spgr.dto;
+
+public class TestDto {
+	private String id;
+	private String subject;
+	private String context;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getContext() {
+		return context;
+	}
+	public void setContext(String context) {
+		this.context = context;
+	}
+	
+	
+}
